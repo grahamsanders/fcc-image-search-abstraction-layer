510b4b119ef5cbedc556a1d11310afd685ee63a6
Graham Sanders <grahamsanders@gmail.com>
2017-08-07 01:02:37 +0000

    Added /latest feature

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+'use strict'
+
 const express = require('express');
+const mongodb = require('mongodb');
 const googleapis = require('googleapis');
 const app = express();
 const customsearch = googleapis.customsearch('v1');
 
+const DATABASE_URI = 'mongodb://user:' + process.env.MONGOPASS + '@ds147872.mlab.com:47872/freecodecamp';
+const DATABASE_COLLECTION = 'fcc-image-search-abstraction-layer';
+
 const CX = process.env.GCSE_CX;
 const API_KEY = process.env.GCSE_API_KEY;
 
-app.get('/favicon.ico', function(request, response) {
+let database;
+
+app.get('/favicon.ico', (request, response) => {
   response.status(204).end();
 });
 
@@ -31,29 +39,46 @@ app.get('/search/:searchstring', (request, response) => {
         "page_url" : item.image.contextLink,
       };
     });
+
+    let collection = database.collection(DATABASE_COLLECTION);
+    collection.save( {
+      "search_term" : request.params.searchstring,
+      "time" : new Date().toISOString(),
+    });
+
     response.status(200).send( items );
   });
 });
 
 app.get('/latest', (request, response) => {
-  response.status(500).send([ { "error" : "error" } ]);
+  let collection = database.collection(DATABASE_COLLECTION);
+  let thing = collection.find({}, {"_id": false}).limit(10).sort({"_id" : -1}).toArray().then((documents) => {
+    response.status(200).send(documents);
+  }).catch( (error) => {
+    response.status(500).send(error);
+  });
 });
 
 // Respond not found to all the wrong routes
-app.use(function(req, res, next){
+app.use( (req, res, next) => {
   res.status(404);
   res.type('txt').send('Not found');
 });
 
 // Error Middleware
-app.use(function(err, req, res, next) {
+app.use( (err, req, res, next) => {
   if(err) {
     res.status(err.status || 500)
       .type('txt')
       .send(err.message || 'SERVER ERROR');
   }  
-})
+});
 
-app.listen(process.env.PORT, function () {
-  console.log('Node.js listening on ' + process.env.PORT + '...');
+mongodb.MongoClient.connect(DATABASE_URI, (error, db) => {
+  if (error) throw error;
+
+  database = db;
+  app.listen(process.env.PORT, () => {
+    console.log('Node.js listening on ' + process.env.PORT + '...');
+  });
 });
